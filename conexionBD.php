a8d1af0d79c12f441864d4955b1aa3dd8015b197
eat.94.15 <eat.94.15@gmail.com>
2016-10-26 15:06:45 -0400

    se agrego composer y phpmailer

---
+++ conexionBD.php
@@ -0,0 +1,6 @@
+<?php
+		$datosConexionBD[0] = 'mysql.red-7.com.mx'; // Colocamos el servidor donde está MySql
+		$datosConexionBD[1] = 'admin'; // Colocamos el usuario de Mysql
+		$datosConexionBD[2] = '7R@server'; // Colocamos la contraseña del usuario de Mysql
+		$datosConexionBD[3] = 'nucleosales'; // Colocamos el nombre de la BD
+?>
--- index.php
+++ index.php
@@ -1,3 +1,37 @@
 <?php
-echo "si";
+require 'vendor/phpmailer/phpmailer/PHPMailerAutoload.php';
+
+$mail = new PHPMailer;
+
+//$mail->SMTPDebug = 3;                               // Enable verbose debug output
+
+$mail->isSMTP();                                      // Set mailer to use SMTP
+$mail->Host = 'smtp.1and1.mx';  // Specify main and backup SMTP servers
+$mail->SMTPAuth = true;                               // Enable SMTP authentication
+$mail->Username = 'contacto@eduardoabad.com';         // SMTP username
+$mail->Password = 'secret';                           // SMTP password
+$mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
+$mail->Port = 587;                                    // TCP port to connect to
+
+$mail->setFrom('contacto@eduardoabad.com', 'Mailer');
+$mail->addAddress('abad.94.15@gmail.com', 'ABAD');     // Add a recipient
+$mail->addAddress('eat.94.15@gmail.com');               // Name is optional
+$mail->addReplyTo('contacto@eduardoabad.com', 'Information');
+$mail->addCC('eduardo.abad@uabc.edu.mx');
+$mail->addBCC('eduardo.abad@uabc.edu.mx');
+
+$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+$mail->isHTML(true);                                  // Set email format to HTML
+
+$mail->Subject = 'Here is the subject';
+$mail->Body    = 'This is the HTML message body <b>in bold!</b>';
+$mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+if(!$mail->send()) {
+    echo 'Message could not be sent.';
+    echo 'Mailer Error: ' . $mail->ErrorInfo;
+} else {
+    echo 'Message has been sent';
+}
 ?>
